849e37cf79bfbda9a17c52e3444828bc89cac39c
lxowalle <83055338+lxowalle@users.noreply.github.com>
2026-04-03 10:12:15 +0800

    * Load zoneinfo from TZ and ZONEINFO env (#2279)

--- cmd/picoclaw/main.go
+++ cmd/picoclaw/main.go
@@ -9,6 +9,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/spf13/cobra"
 
@@ -68,6 +69,21 @@ const (
 
 func main() {
 	fmt.Printf("%s", banner)
+
+	tz_env := os.Getenv("TZ")
+	if tz_env != "" {
+		fmt.Println("TZ environment:", tz_env)
+		zoneinfo_env := os.Getenv("ZONEINFO")
+		fmt.Println("ZONEINFO environment:", zoneinfo_env)
+		loc, err := time.LoadLocation(tz_env)
+		if err != nil {
+			fmt.Println("Error loading time zone:", err)
+		} else {
+			fmt.Println("Time zone loaded successfully:", loc)
+			time.Local = loc //nolint:gosmopolitan // We intentionally set local timezone from TZ env
+		}
+	}
+
 	cmd := NewPicoclawCommand()
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
